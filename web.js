21cb50f9977d9d5ace66a1c802cb377987aca4d6
tf292201 <tf292201@gmail.com>
2023-11-01 17:51:55 -0400

    Initial Commit

---
+++ web.js
@@ -0,0 +1,19 @@
+// 1. hyper text transfer protocol, it is a protocol for how data is sent and
+//    received over the web.a
+// 2. Universal Resource locator. It is a reference to a web resource.It
+// 3. Domain name system, It takes a domain name and returns an IP address.
+// 4. Query strings are used to pass information through the URL. 
+// 5. GET - Requests data from the server.
+//    POST - Submits data to the server.
+// 6. http requests are sent to the server to request or submit data.
+// 7. http responses are sent back to the client from the server.
+// 8. Headers contain key value PerformanceObserverEntryList, usually extra information about the request.
+//    cookie Header,  content type header. 
+// 9. THe URL DNS server translates the domain name into an IP address.
+//    The browser then sends an HTTP request to the server at that IP address.
+//    The server sends back an HTTP response. This contains the requested data such as HTML, CSS, and JS files.
+//    The browser then renders these files into the webpage you see on the screen.
+
+ 
+//curl -H "Accept: application/json" "https://icanhazdadjoke.com/search?term=pirate"
+
